e90a4f8f2c808249516cfba47ab4a11a39dc20b4
theultracorgi <math4ken@gmail.com>
2017-04-05 17:32:33 -0500

    Pushing

--- FRCScoutingApp/app/src/main/java/com/example/anew/frcscoutingapp/MainMenu.java
+++ FRCScoutingApp/app/src/main/java/com/example/anew/frcscoutingapp/MainMenu.java
@@ -13,7 +13,7 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-
+        onButtonClickListener();
     }
 
         public void onButtonClickListener() {
