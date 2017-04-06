597b73b369b17d1f2bdc40110abbe7c4dbd5a462
theultracorgi <math4ken@gmail.com>
2017-04-06 17:27:43 -0500

    Pushing Changes

---
+++ FRCScoutingApp/app/src/main/java/com/example/anew/frcscoutingapp/QRStager.java
@@ -0,0 +1,13 @@
+package com.example.anew.frcscoutingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QRStager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrstager);
+    }
+}
