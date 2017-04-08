4dc11ad59ca7242fca7fac2403fa8584484a596d
aocampo2862 <aocampo2862@gmail.com>
2017-04-08 15:09:13 -0500

    auton buttons

--- FRCScoutingApp/app/src/main/java/com/example/anew/frcscoutingapp/AutonMenu.java
+++ FRCScoutingApp/app/src/main/java/com/example/anew/frcscoutingapp/AutonMenu.java
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AutonMenu extends AppCompatActivity {
     Button backAutonMenu, goToAuton2, mainMenu;
+    ImageButton addBallsAuton, subtractBallsAuton;
+    Integer numBallsAuton = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +22,8 @@ public class AutonMenu extends AppCompatActivity {
         backAutonMenu = (Button) findViewById(R.id.backAutonMenu);
         goToAuton2 = (Button) findViewById(R.id.goToAuton2);
         mainMenu = (Button) findViewById(R.id.mainMenu);
-
+        addBallsAuton = (ImageButton) findViewById(R.id.addBallsAuton);
+        subtractBallsAuton = (ImageButton) findViewById(R.id.subtractBallsAuton);
 
 
         goToAuton2.setOnClickListener(
@@ -51,6 +55,24 @@ public class AutonMenu extends AppCompatActivity {
                         Intent intent = new Intent(AutonMenu.this, MainMenu.class);
                         startActivity(intent);
 
+                    }
+                }
+
+        );
+        addBallsAuton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        numBallsAuton = numBallsAuton + 1;
+                    }
+                }
+
+        );
+        subtractBallsAuton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        numBallsAuton = numBallsAuton - 1;
                     }
                 }
 
--- FRCScoutingApp/app/src/main/java/com/example/anew/frcscoutingapp/AutonMenu2.java
+++ FRCScoutingApp/app/src/main/java/com/example/anew/frcscoutingapp/AutonMenu2.java
@@ -5,9 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 public class AutonMenu2 extends AppCompatActivity {
     Button backAutonMenu2, goToTeleop, mainMenu;
+    ToggleButton crossBaseline;
+    ImageButton subtractFoulsAuton, addFoulsAuton, subtractTechFoulsAuton, addTechFoulsAuton, subtractRoboErrorsAuton, addRoboErrorsAuton;
+    Integer numFouls, numTechFouls, numRoboErrors;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,13 @@ public class AutonMenu2 extends AppCompatActivity {
         backAutonMenu2 = (Button) findViewById(R.id.backAutonMenu2);
         goToTeleop = (Button) findViewById(R.id.goToTeleop);
         mainMenu = (Button) findViewById(R.id.mainMenu);
+        subtractFoulsAuton = (ImageButton) findViewById(R.id.subtractFoulsAuton);
+        addFoulsAuton = (ImageButton) findViewById(R.id.addFoulsAuton);
+        subtractTechFoulsAuton = (ImageButton) findViewById(R.id.subtractTechFoulsAuton);
+        addTechFoulsAuton = (ImageButton) findViewById(R.id.addTechFoulsAuton);
+        subtractRoboErrorsAuton = (ImageButton) findViewById(R.id.subtractRoboErrorsAuton);
+        addRoboErrorsAuton = (ImageButton) findViewById(R.id.addRoboErrorsAuton);
+        crossBaseline = (ToggleButton) findViewById(R.id.crossBaseline);
 
 
 
@@ -50,6 +62,75 @@ public class AutonMenu2 extends AppCompatActivity {
                     public void onClick(View v) {
                         Intent intent = new Intent(AutonMenu2.this, MainMenu.class);
                         startActivity(intent);
+
+                    }
+                }
+
+        );
+        subtractFoulsAuton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        numFouls = numFouls - 1;
+                    }
+                }
+
+        );
+        addFoulsAuton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        numFouls = numFouls + 1;
+                    }
+                }
+
+        );
+        subtractTechFoulsAuton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        numTechFouls = numTechFouls - 1;
+                    }
+                }
+
+        );
+        addTechFoulsAuton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        numTechFouls = numTechFouls + 1;
+                    }
+                }
+
+        );
+        subtractRoboErrorsAuton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        numRoboErrors = numRoboErrors - 1;
+                    }
+                }
+
+        );
+        addRoboErrorsAuton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        numRoboErrors = numRoboErrors + 1;
+                    }
+                }
+
+        );
+        crossBaseline.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        
 
                     }
                 }
